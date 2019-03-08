dd1a3311b6effc1bb4c446a759e5cba20ebcb2ad
Digya Mandala Putra <digyamp@gmail.com>
2019-03-08 13:56:12 +0700

    firstPush

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <router-link to="/">
+      <img src="~assets/blibli-logo.png" alt="blibli-logo">
+    </router-link>
+
+    <div style="margin: 30px;">
+      <router-link to="/contact-us" class="button">
+        Contact Us
+      </router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.button{
+
+  background-color: lightblue;
+  padding: 10px;
+  border-radius: 5px;
+  text-decoration: none;
+  color: black;
+  font-weight: bold;
+}
+
+.content{
+  padding-top: 20px;
+}
+</style>
---
+++ src/pages/ContactUsPage.vue
@@ -0,0 +1,65 @@
+<template>
+import { METHODS } from 'http';
+    <div>
+        <div>
+            <label for="">
+                Full Name:
+                <input
+                    type="text"
+                    class="contact-input"
+                    v-model="formData.fullName"
+                >
+            </label>
+        </div>
+        <div>
+            <label for="">
+                Email:
+                <input type="text"
+                v-model="formData.email">
+            </label>
+        </div>
+        <div>
+            <label for="">
+                Description:
+                <textarea
+                v-model="formData.description"></textarea>
+            </label>
+        </div>
+
+        <div>
+            <button>Submit</button>
+        </div>
+
+        <div>
+            Data:
+            {{formData}}
+            <!-- <div>
+                FullName: {{fullName}}
+            </div> -->
+        </div>
+    </div>
+</template>
+
+<script type="text/javascript">
+export default {
+    name: 'ContactUsPage',
+    data(){
+        return{
+            formData:{
+                fullName: '',
+                email: '',
+                description: '',
+            }
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+label{
+    display: block;
+    padding-top: 20px
+}
+</style>
+
